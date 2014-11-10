9c733421e623215636443425e90b5918e24d2d70
unknown <c.cavanna@po260-201402.mb-diffusion.lan>
2014-11-10 18:38:29 +0100

    Add BDD

---
+++ bdd.php
@@ -0,0 +1,7 @@
+<?php 
+$dbname = "raging-ghoul-v1";
+$user   = "root";
+$pass   = "";
+
+$db = new PDO('mysql:host=localhost;dbname=' . $dbname, $user, $pass);
+?>
--- candidature.php
+++ candidature.php
@@ -1,8 +1,7 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+include './bdd.php';
 
+$_POST[''];
+
+$bdd->exec("INSERT INTO membres(champ_login,champ_mdp) VALUES('login','mot_de_passe')");
