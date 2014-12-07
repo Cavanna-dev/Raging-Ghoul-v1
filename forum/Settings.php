cbd6217b705cc72f7218381c8758147fa2b312ad
KeurbyCandy <cavannachristophe@gmail.com>
2014-12-07 17:13:49 +0100

    - Installation Forum

--- forum/Settings.php
+++ forum/Settings.php
@@ -18,16 +18,16 @@ $mtitle = 'Maintenance Mode';		# Title for the Maintenance Mode message.
 $mmessage = 'Okay faithful users...we\'re attempting to restore an older backup of the database...news will be posted once we\'re back!';		# Description of why the forum is in maintenance mode.
 
 ########## Forum Info ##########
-$mbname = 'My Community';		# The name of your forum.
-$language = 'english';		# The default language file set for the forum.
-$boardurl = 'http://127.0.0.1/smf';		# URL to your forum's folder. (without the trailing /!)
-$webmaster_email = 'noreply@myserver.com';		# Email address to send emails from. (like noreply@yourdomain.com.)
-$cookiename = 'SMFCookie11';		# Name of the cookie to set for authentication.
+$mbname = 'Raging Ghoul';		# The name of your forum.
+$language = 'french-utf8';		# The default language file set for the forum.
+$boardurl = 'http://local.rg-guild.eu/forum';		# URL to your forum's folder. (without the trailing /!)
+$webmaster_email = 'cavannachristophe@gmail.com';		# Email address to send emails from. (like noreply@yourdomain.com.)
+$cookiename = 'SMFCookie723';		# Name of the cookie to set for authentication.
 
 ########## Database Info ##########
 $db_type = 'mysql';
-$db_server = 'localhost';
-$db_name = 'smf';
+$db_server = 'localhost:3306';
+$db_name = 'raging-ghoul-v1';
 $db_user = 'root';
 $db_passwd = '';
 $ssi_db_user = '';
@@ -38,18 +38,14 @@ $db_error_send = 1;
 
 ########## Directories/Files ##########
 # Note: These directories do not have to be changed unless you move things.
-$boarddir = dirname(__FILE__);		# The absolute path to the forum's folder. (not just '.'!)
-$sourcedir = dirname(__FILE__) . '/Sources';		# Path to the Sources directory.
-$cachedir = dirname(__FILE__) . '/cache';		# Path to the cache directory.
+$boarddir = 'E:\\wamp\\www\\Raging-Ghoul-v1\\forum';		# The absolute path to the forum's folder. (not just '.'!)
+$sourcedir = 'E:\\wamp\\www\\Raging-Ghoul-v1\\forum/Sources';		# Path to the Sources directory.
+$cachedir = 'E:\\wamp\\www\\Raging-Ghoul-v1\\forum/cache';		# Path to the cache directory.
 
 ########## Error-Catching ##########
 # Note: You shouldn't touch these settings.
 $db_last_error = 0;
 
-if (file_exists(dirname(__FILE__) . '/install.php'))
-{
-	header('Location: http' . (!empty($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) == 'on' ? 's' : '') . '://' . (empty($_SERVER['HTTP_HOST']) ? $_SERVER['SERVER_NAME'] . (empty($_SERVER['SERVER_PORT']) || $_SERVER['SERVER_PORT'] == '80' ? '' : ':' . $_SERVER['SERVER_PORT']) : $_SERVER['HTTP_HOST']) . (strtr(dirname($_SERVER['PHP_SELF']), '\\', '/') == '/' ? '' : strtr(dirname($_SERVER['PHP_SELF']), '\\', '/')) . '/install.php'); exit;
-}
 
 # Make sure the paths are correct... at least try to fix them.
 if (!file_exists($boarddir) && file_exists(dirname(__FILE__) . '/agreement.txt'))
@@ -59,4 +55,5 @@ if (!file_exists($sourcedir) && file_exists($boarddir . '/Sources'))
 if (!file_exists($cachedir) && file_exists($boarddir . '/cache'))
 	$cachedir = $boarddir . '/cache';
 
+$db_character_set = 'utf8';
 ?>
