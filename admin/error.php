26ddc03e04ce04b94ec6a553e2cd724e50710d0b
KeurbyCandy <cavannachristophe@gmail.com>
2015-01-02 19:02:32 +0100

    E-notice

--- admin/error.php
+++ admin/error.php
@@ -3,31 +3,10 @@
     <head>
         <meta charset="UTF-8">
         <title>Raging Ghoul</title>
-        <link rel="stylesheet" href="http://bootswatch.com/darkly/bootstrap.min.css" type="text/css" media="screen" />
-        <link rel="stylesheet" href="./css/main.css" type="text/css" />
+        <?php include './cssFiles.php'; ?>
     </head>
     <body>
-        <div class="navbar navbar-default" style="margin:0">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">Raging Ghoul</a>
-                </div>
-                <div class="navbar-collapse collapse navbar-responsive-collapse">
-                    <ul class="nav navbar-nav">
-                        <li><a href="./index.php">Accueil</a></li>
-                    </ul>
-                    <ul class="nav navbar-nav navbar-right">
-                        <li class="active"><a href="./postuler.php">Postuler</a></li>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
+        <?php include './menu.php'; ?>
         <div id="bodybg">
             <div class="container">
                 <div id="bodycontent" class="jumbotron">
--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,7 @@
 <?php
+error_reporting(E_ALL & ~E_NOTICE);
 session_start();
-if ($_SESSION['login'] == 'admin')
+if (!empty($_SESSION['login']))
     header('location:recruit.php');
 else {
     ?>
